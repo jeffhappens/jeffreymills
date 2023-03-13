8982d38748c0993b62d2e1c1eecceec1b66c2b7c
Jeff Mills <blatant3@gmail.com>
2023-03-13 12:14:56 -0400

    build

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
             </aside>
             <section class="w-3/4 text-7xl font-bold">
                 <h1 class="mb-6">I develop cutting edge websites and apps using modern tools like Laravel and VueJS.</h1>
-                <p class="text-2xl text-amber-500">I can turn your design into a working product.</p>
+                <p class="text-2xl text-amber-500">I will turn your design into a working product.</p>
             </section>
             
         </main-container>
