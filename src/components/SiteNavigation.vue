2838ffbe414313bf08a34482c6bce33bc7b681a1
Jeff Mills <blatant3@gmail.com>
2023-03-13 15:38:49 -0400

    Add homepage animations

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -5,19 +5,21 @@
 <template>
     <main-container class="h-12 w-full font-bold flex items-center justify-between">
         <p class="text-xl">
-            <router-link class="home" to="/">Portfolio.</router-link>
+            <router-link class="home" to="/">Jeff Mills / <span class="text-amber-500">Developer Portfolio.</span></router-link>
         </p>
         <ul class="flex items-center gap-4 text-gray-500 font-normal">
-            <li>
-                <router-link to="/work">Work</router-link>
-            </li>
+            <!-- <li><router-link to="/work">Work</router-link></li>
             <li><router-link to="/about">About</router-link></li>
-            <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="/contact">Contact</router-link></li> -->
             <li class="ml-8">
-                <font-awesome-icon class="text-linkedin-500 text-3xl" icon="fa-brands fa-linkedin" />
+                <a href="" title="View my LinkedIn Profile">
+                    <font-awesome-icon class="text-linkedin-500 text-3xl" icon="fa-brands fa-linkedin" />
+                </a>
             </li>
             <li>
-                <font-awesome-icon class="text-github-500 text-3xl" icon="fa-brands fa-square-github" />
+                <a href="" title="View my Github Profile">
+                    <font-awesome-icon class="text-github-500 text-3xl" icon="fa-brands fa-square-github" />
+                </a>
             </li>
         </ul>
     </main-container>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="home relative overflow-x-hidden">
-        <div class="absolute top-1/3 -rotate-6 left-0 -z-10 w-full h-48 bg-gray-100"></div>
+
+        <div class="bg">
+            <div class="rect"></div>
+        </div>
 
         <main-container class="flex items-center h-[calc(100vh-3rem)] font-semibold text-3xl">
             
-            <aside class="flex-1">
+            <aside class="flex-1 greeting">
                 <p>Hi, I'm Jeff</p>
             </aside>
-            <section class="w-3/4 text-7xl font-bold">
+            <section class="w-3/4 text-7xl font-bold message">
                 <h1 class="mb-6">I develop cutting edge websites and apps using modern tools like Laravel and VueJS.</h1>
-                <p class="text-2xl text-amber-500">I will turn your design into a working product.</p>
+                <p class="text-2xl text-gray-500">I can turn your design into a working product.</p>
 
             </section>
             
@@ -25,3 +28,95 @@
         //   name: 'HomeView',
     // }
 </script>
+
+<style scoped>
+
+.greeting {
+    opacity: 0;
+    animation-name: slideDown;
+    animation-delay: .15s;
+    animation-duration: 1.5s;
+    animation-fill-mode: forwards;
+    animation-direction: alternate;
+}
+.message {
+    opacity: 0;
+    animation-name: slideInLeft;
+    animation-delay: .15s;
+    animation-duration: 1.5s;
+    animation-fill-mode: forwards;
+    animation-direction: alternate;
+}
+.bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -10;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+.rect {
+    transition: .3s ease all;
+    width: 100%;
+    height: 0;
+    background: #eee;
+
+
+
+    animation-name: fullHeight;
+    animation-duration: 500ms;
+    animation-delay: 250ms; /* add this */
+    animation-timing-function: cubic-bezier(0.04, 0.65, 0.2, 1);
+    animation-fill-mode: forwards;
+    animation-iteration-count: 1;
+
+
+
+}
+
+@keyframes fullWidth {
+    0% {
+        width: 25%;
+    }
+    100% {
+        width: 85%;
+
+    }
+}
+@keyframes fullHeight {
+    from {
+        height: 25%;
+    }
+    to {
+        height: 100%;
+    }
+}
+
+
+@keyframes slideDown {
+    from {
+        opacity: 0;
+        transform: translateY(-200%);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+@keyframes slideInLeft {
+    from {
+        opacity: 0;
+        transform: translateY(15%);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+</style>
