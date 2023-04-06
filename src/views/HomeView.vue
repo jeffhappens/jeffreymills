7dd96b94abd9f937c70a367ec5b9ab24641d83c4
Jeff Mills <blatant3@gmail.com>
2023-04-06 14:54:05 -0400

    text change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@
             <section class="message font-bold text-5xl md:text-6xl lg:text-7xl max-w-4xl">
                 <h1>I develop cutting edge websites and apps using modern tools like Laravel and VueJS.</h1>
                 <aside class="subtext mt-4 text-lg text-amber-500 font-semibold lg:text-xl">
-                    <p>And I can help turn your next design into a working product.</p>
+                    <p>Let's turn your next design into a working product.</p>
                 </aside>
             </section>
 
