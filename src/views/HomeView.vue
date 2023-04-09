e422e4d150525297f78392e848d653b47ac11045
jeffhappens <blatant3@gmail.com>
2023-04-09 13:06:25 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,138 +1,131 @@
 <template>
-    <div class="home relative overflow-x-hidden">
-
-        <div class="bg">
-            <div class="rect"></div>
-        </div>
-
-        <main-container class="h-[calc(100vh-3.5rem)] flex flex-col justify-evenly">
-        
-            
-            <aside class="greeting font-bold text-2xl text-gray-600 lg:text-3xl">
-                <p>Hi, I'm Jeff</p>
-            </aside>
-            <section class="message font-bold text-5xl md:text-6xl lg:text-7xl max-w-4xl">
-                <h1>I develop cutting edge websites and apps using modern tools like Laravel and VueJS.</h1>
-                <aside class="subtext mt-4 text-lg text-amber-500 font-semibold lg:text-xl">
-                    <p>Let's turn your next design into a working product.</p>
-                </aside>
-            </section>
-
-            
-        </main-container>
+  <div class="home relative overflow-x-hidden">
+    <div class="bg">
+      <div class="rect"></div>
     </div>
+
+    <main-container class="h-[calc(100vh-3.5rem)] flex flex-col justify-evenly">
+      <aside class="greeting font-bold text-2xl text-gray-600 lg:text-3xl">
+        <p>Hi, I'm Jeff</p>
+      </aside>
+      <section
+        class="message font-bold text-5xl md:text-6xl lg:text-7xl max-w-4xl"
+      >
+        <h1>
+          I develop cutting edge websites and apps using modern tools like
+          Laravel and VueJS.
+        </h1>
+        <aside
+          class="subtext mt-4 text-lg text-amber-500 font-semibold lg:text-xl"
+        >
+          <p>Let's turn your next design into a working product.</p>
+        </aside>
+      </section>
+    </main-container>
+  </div>
 </template>
 
 <script setup>
-    import MainContainer from '../components/layout/MainContainer.vue'
-    // @ is an alias to /src
+import MainContainer from "../components/layout/MainContainer.vue";
+// @ is an alias to /src
 
-    // export default {
-        //   name: 'HomeView',
-    // }
+// export default {
+//   name: 'HomeView',
+// }
 </script>
 
 <style scoped>
-
 .greeting,
 .message,
 .subtext {
-    opacity: 0;
-    animation-delay: 1500ms;
-    animation-duration: 1.5s;
-    animation-fill-mode: forwards;
-    animation-direction: alternate;
+  opacity: 0;
+  animation-delay: 1500ms;
+  animation-duration: 1.5s;
+  animation-fill-mode: forwards;
+  animation-direction: alternate;
 }
 .greeting {
-    animation-name: slideInLeft;
+  animation-name: slideInLeft;
 }
 .message {
-    animation-name: slideInRight;
+  animation-name: slideInRight;
 }
 .subtext {
-    animation-name: slideInBottom;
+  animation-name: slideInBottom;
 }
 .bg {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -10;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -10;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 .rect {
-    transition: .3s ease all;
-    /* width: 100%; */
-    height: 5px;
-    background: #f8f8f8;
+  transition: 0.3s ease all;
+  /* width: 100%; */
+  height: 5px;
+  background: #f8f8f8;
 
-    animation-name: fullWidth, fullHeight;
-    animation-duration: 750ms;
-    animation-delay: 250ms, 1000ms; /* add this */
-    animation-timing-function: cubic-bezier(0.04, 0.65, 0.2, 1);
-    animation-fill-mode: forwards;
-    animation-iteration-count: 1;
-
-
-
+  animation-name: fullWidth, fullHeight;
+  animation-duration: 750ms;
+  animation-delay: 250ms, 1000ms; /* add this */
+  animation-timing-function: cubic-bezier(0.04, 0.65, 0.2, 1);
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
 }
 
 @keyframes fullWidth {
-    0% {
-        width: 0%;
-    }
-    100% {
-        width: 100%;
-
-    }
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
 }
 @keyframes fullHeight {
-    from {
-        height: 5px;
-    }
-    to {
-        height: 100%;
-    }
+  from {
+    height: 5px;
+  }
+  to {
+    height: 100%;
+  }
 }
 
-
 @keyframes slideInLeft {
-    from {
-        opacity: 0;
-        transform: translateX(-2%);
-    }
-    to {
-        opacity: 1;
-        transform: translateX(0);
-    }
+  from {
+    opacity: 0;
+    transform: translateX(-2%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 }
 
 @keyframes slideInRight {
-    from {
-        opacity: 0;
-        transform: translateX(5%);
-    }
-    to {
-        opacity: 1;
-        transform: translateX(0);
-    }
+  from {
+    opacity: 0;
+    transform: translateX(5%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 }
 
 @keyframes slideInBottom {
-    from {
-        opacity: 0;
-        transform: translateY(150%);
-    }
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
+  from {
+    opacity: 0;
+    transform: translateY(150%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
-
-
-
 </style>
