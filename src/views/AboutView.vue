29b8d15a035150b0c5cf038fac82c709962e488b
Jeff Mills <blatant3@gmail.com>
2023-04-12 09:56:41 -0400

    Add new views

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,5 +3,9 @@
 </script>
 
 <template>
-    <main-container></main-container>
+    <main-container>
+        <div class="w-1/2 my-8 py-2 border-b-2 border-amber-400">
+            <h1 class="text-4xl font-semibold">About Me</h1>
+        </div>        
+    </main-container>
 </template>
