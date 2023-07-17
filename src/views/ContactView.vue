424c88ab14731f5addaaaff3a37f45bc061d49d6
Jeff Mills <blatant3@gmail.com>
2023-07-17 15:15:08 -0400

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -6,5 +6,22 @@
 <template>
     <main-container>
         <PageHeadline text="Let's get in touch." />
+
+        <div>
+            <p class="my-10 text-3xl text-gray-500">
+                I would love to talk to you about your next project!
+                <br>
+                Please get in touch with me in one of the following ways:</p>
+            <ul class="text-3xl text-gray-700">
+                <li class="my-4">
+                    <span class="text-amber-500 font-bold block md:inline-block w-[100px]">Cell:</span>
+                    <a class="" href="TEL:4439446597">(443)-944-6597</a>
+                </li>
+                <li class="my-2">
+                    <span class="text-amber-500 font-bold block md:inline-block w-[100px]">Email:</span>
+                    <a href="mailto:jeffrey.mills@outlook.com">jeffrey.mills@outlook.com</a>
+                </li>
+            </ul>
+        </div>
     </main-container>
 </template>
